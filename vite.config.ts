5d22b3a74034e42297bfcedb8ce6fe0759c922e3
HezerSantos <HezerSantos24@gmail.com>
2025-06-30 23:56:40 -0500

    installed terser

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-    minify: 'esbuild',
-    sourcemap: false,
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      mangle: {
+        keep_fnames: true, // prevents function name mangling
+      },
+    },
+    sourcemap: true, // enable source maps for debugging
   },
   server: {
     allowedHosts: ['fae5-75-48-60-225.ngrok-free.app'],
     port: 5173,
     host: '0.0.0.0'
   },
-
 })
