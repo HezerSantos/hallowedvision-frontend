7194a4f2ef8f5debbeed035f5043a2bd3b34d173
HezerSantos <HezerSantos24@gmail.com>
2025-07-02 23:58:19 -0500

    Created the side nav

--- src/components/universal/navBar.tsx
+++ src/components/universal/navBar.tsx
@@ -1,14 +1,63 @@
-import React from "react"
+import React, { useRef } from "react"
 import logo from '../../assets/images/logo.webp'
+import { Link, useLocation } from "react-router-dom"
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 const NavBar: React.FC = () => {
+    const location = useLocation()
+    const sideBar = useRef<HTMLElement>(null)
+
+    const toggleSideNav = ():void => {
+        if(sideBar.current?.classList.contains("toggle")){
+            sideBar.current?.classList.remove("toggle")
+        } else {
+            sideBar.current?.classList.add("toggle")
+        }
+    }
     return(
         <>
-            <nav className="page-section">
+            <aside className="sidebar-nav toggle" ref={sideBar}>
+                <nav>
+                    <div className="sidebar-nav__header">
+                        <div>
+                            <img src={logo} alt="logo"/>
+                            <h1>Hallowed Visions</h1>
+                        </div>
+                        <button onClick={() => toggleSideNav()}>
+                            CLOSE
+                        </button>
+                    </div>
+                    <div className="sidebar-nav__content">
+                        <ul>
+                            <li><Link to="" className={location.pathname === "/"? "selected" : ""}>Home</Link></li>
+                            <li><Link to="" className={location.pathname === "/portfolio"? "selected" : ""}>Portfolio</Link></li>
+                        </ul>
+                    </div>
+                    <div className="sidebar-nav__footer">
+                        <p>LET'S CREATE TOGETHER</p>
+                        <h1>contact@hallowedvisions.com</h1>
+                        <ul>
+                            <li>
+                                <button>
+                                    <FaInstagram />
+                                </button>
+                            </li>
+                            <li>
+                                <button>
+                                    <FaLinkedin />
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </aside>
+            <nav className="page-section nav-main-wrapper">
                 <div className="nav-main page-section__child">
                     <div className="nav-main__header">
                         <img src={logo} alt="" />
                         <p>Hallowed Visions</p>
-                        <button className="nav-side-toggle">
+                        <button className="nav-side-toggle" onClick={() => toggleSideNav()}>
                             <div></div>
                             <div></div>
                         </button>
@@ -29,6 +78,8 @@ const NavBar: React.FC = () => {
                     </div>
                 </div>
             </nav>
+
+
         </>
     )
 }
