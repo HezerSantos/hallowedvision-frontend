e27ed44b54b6508299a36fa0efba257ec090acbb
HezerSantos <HezerSantos24@gmail.com>
2025-07-05 15:08:35 -0500

    Progress on portfolio details

---
+++ src/components/portfolio/portfolioDetails.tsx
@@ -0,0 +1,11 @@
+const PortfolioDetails: React.FC = () => {
+    return(
+        <>
+            <section className="page-section__child portfolio-details">
+
+            </section>
+        </>
+    )
+}
+
+export default PortfolioDetails
--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -2,11 +2,15 @@ import React from "react"
 import NavBar from "../../components/universal/navBar"
 import '../../assets/styles/portfolio/portfolio.css'
 import PortfolioHeader from "../../components/portfolio/portfolioHeader"
+import PortfolioDetails from "../../components/portfolio/portfolioDetails"
 const Portfolio: React.FC = () => {
     return(
         <>
             <NavBar />
             <PortfolioHeader />
+            <main className="page-section">
+                <PortfolioDetails />
+            </main>
         </>
     )
 }
