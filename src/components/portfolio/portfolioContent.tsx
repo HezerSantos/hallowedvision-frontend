e44b13e1d9b7e46329485b9daf0087b58db8cabc
HezerSantos <HezerSantos24@gmail.com>
2025-07-07 01:32:01 -0500

    completed the basis for the portfolio page

---
+++ src/components/portfolio/portfolioContent.tsx
@@ -0,0 +1,121 @@
+import React from "react"
+import PortfolioSectionHeader from "./portfolioSectionHeader"
+import testImage from '../../assets/images/testImage.png'
+import { Link } from "react-router-dom"
+
+import { FaHtml5 } from "react-icons/fa6";
+import { FaCss3Alt } from "react-icons/fa6";
+import { FaJs } from "react-icons/fa6";
+import { FaReact } from "react-icons/fa6";
+import { SiExpress } from "react-icons/si";
+import { FaNode } from "react-icons/fa6";
+import { FaPython } from "react-icons/fa6";
+import { DiMysql } from "react-icons/di";
+
+
+interface PortfolioItemProps {
+    imageUrl: string,
+    title: string,
+    type: string,
+    demoUrl: string,
+    technologies: string[],
+    description: string
+}
+
+const technologyMap: Map<string, React.ReactElement> = new Map()
+
+
+technologyMap.set("HTML", <FaHtml5 />)
+technologyMap.set("CSS", <FaCss3Alt />)
+technologyMap.set("JavaScript", <FaJs />)
+technologyMap.set("ReactJS", <FaReact />)
+technologyMap.set("Express.js", <SiExpress />)
+technologyMap.set("Node.js", <FaNode />)
+technologyMap.set("Python", <FaPython />)
+technologyMap.set("SQL", <DiMysql />)
+
+
+interface TechnologyItemProps {
+    techName: string,
+    icon: React.ReactElement | undefined
+}
+const TechnologyItem: React.FC<TechnologyItemProps> = ({techName, icon}) => {
+    return(
+        <>
+            <div className="technology-item">
+                {icon}
+                <p>{techName}</p>
+            </div>
+        </>
+    )
+}
+
+const PortfolioItem: React.FC<PortfolioItemProps> = ({imageUrl, title, type, demoUrl, technologies, description}) => {
+    return(
+        <>
+            <div className="portfolio-item">
+                <div>
+                    <img src={imageUrl} alt={title} />
+                </div>
+                <div className="portfolio-item__description">
+                    <div className="portfolio-item__description-header">
+                        <h1>{title} | {type} Application</h1>
+                        <Link to={demoUrl}>Live Demo</Link>
+                    </div>
+                    <div className="portfolio-item__description-technology">
+                        <h2>Technologies</h2>
+                        <div>
+                            {technologies.map((technology, index) => {
+                                const element: React.ReactElement | undefined = technologyMap.get(technology)
+                                return(
+                                    <TechnologyItem 
+                                        key={index}
+                                        icon={element}
+                                        techName={technology}
+                                    />
+                                )
+                            })}
+                        </div>
+                    </div>
+                    <div className="portfolio-item__description-description">
+                        <h2>Description</h2>
+                        <p>{description}</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+const PortfolioContent: React.FC = () => {
+    return (
+        <>
+            <section className="page-section__child portfolio-content">
+                <PortfolioSectionHeader header="From Idea to Execution"/>
+                <div className="portfolio-content__container">
+                    <PortfolioItem 
+                        // key={"PI1"}
+                        imageUrl={testImage}
+                        title="SAHNTEK"
+                        type="FULL-STACK"
+                        demoUrl=""
+                        technologies={["HTML", "CSS", "JavaScript", "ReactJS", "Express.js", "Node.js", "Python", "SQL"]}
+                        description="Full-featured E-commerce application built to sell computers with speed, security, and reliability. Developed using React for a smooth, responsive user experience across all devices, the platform integrates Stripe for seamless and secure payments, and PostgreSQL for robust data management. It includes CSRF protection and industry-standard security practices to ensure safe transactions"
+                    />
+                    <PortfolioItem 
+                        // key={"PI2"}
+                        imageUrl={testImage}
+                        title="SAHNTEK"
+                        type="FULL-STACK"
+                        demoUrl=""
+                        technologies={["HTML", "CSS", "JavaScript", "ReactJS", "Express.js", "Node.js", "Python", "SQL"]}
+                        description="Full-featured E-commerce application built to sell computers with speed, security, and reliability. Developed using React for a smooth, responsive user experience across all devices, the platform integrates Stripe for seamless and secure payments, and PostgreSQL for robust data management. It includes CSRF protection and industry-standard security practices to ensure safe transactions"
+                    />
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default PortfolioContent
--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -3,6 +3,8 @@ import NavBar from "../../components/universal/navBar"
 import '../../assets/styles/portfolio/portfolio.css'
 import PortfolioHeader from "../../components/portfolio/portfolioHeader"
 import PortfolioDetails from "../../components/portfolio/portfolioDetails"
+import PortfolioContent from "../../components/portfolio/portfolioContent"
+import FooterHV from "../../components/universal/footer"
 const Portfolio: React.FC = () => {
     return(
         <>
@@ -10,7 +12,11 @@ const Portfolio: React.FC = () => {
             <PortfolioHeader />
             <main className="page-section">
                 <PortfolioDetails />
+                <PortfolioContent />
             </main>
+            <footer className="page-section portfolio__footer">
+                <FooterHV />
+            </footer>
         </>
     )
 }
