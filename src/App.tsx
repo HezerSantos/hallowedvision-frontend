3f5b81182f83a05840720b97572db870186f3a3d
HezerSantos <HezerSantos24@gmail.com>
2025-07-10 21:44:07 -0500

    Progress on csrf

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import { useLocation } from 'react-router-dom';
+import api from './app.config';
+import axios from 'axios'
+
 function App() {
+  axios.defaults.withCredentials = true
   useEffect(() => {
     if (import.meta.env.MODE === 'production') {
       console.warn = () => {}
@@ -10,13 +14,24 @@ function App() {
       console.debug = () => {}
     }
   }, [])
-
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
+  useEffect(() => {
+    const getCsrf = async() => {
+      try{
+        await axios.get(`${api.url}/api/csrf`)
+        console.log(document.cookie)
+      } catch(e){
+        console.error(e)
+      }
+    }
+
+    getCsrf()
+  }, [])
   return (
     <>
       <Outlet />
