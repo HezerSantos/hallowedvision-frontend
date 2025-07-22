4296aaf80c50741cb23fdef88e20997a35d4ff12
HezerSantos <HezerSantos24@gmail.com>
2025-07-22 14:40:43 -0500

    Modified the contact page to send emails and added proper error handling. Modified some css too

--- src/components/contact/contactContent.tsx
+++ src/components/contact/contactContent.tsx
@@ -1,8 +1,13 @@
-import React from "react"
+import React, { useContext, useState } from "react"
 import { FaPeopleGroup } from "react-icons/fa6";
 import { RiMicroscopeFill } from "react-icons/ri";
 import { FaHandsHelping } from "react-icons/fa";
 import { FaPuzzlePiece } from "react-icons/fa6";
+import CsrfContext from "../../context/csrf/csrfContext";
+import axios from "axios";
+import { AxiosError } from "axios"
+import api from "../../app.config";
+import { AiOutlineLoading } from "react-icons/ai";
 interface InformationItemProps {
     icon: React.ReactElement,
     text: string
@@ -21,23 +26,167 @@ const InformationItem: React.FC<InformationItemProps> = ({icon, text}) => {
 interface FormInputElementProps {
     text: string,
     name: string,
-
+    emailErrors: Map<string, string> | null,
+    isLoading: boolean
 }
 
-const FormInputElement: React.FC<FormInputElementProps> = ({text, name}) => {
+const FormInputElement: React.FC<FormInputElementProps> = ({text, name, emailErrors, isLoading}) => {
     return(
         <>
             <div className="contact__form-input-element">
                 <label htmlFor={name}>{text}</label>
                 <div className="contact__form-input-wrapper">
-                    <input type="text" id={name}/>
+                    <input className={emailErrors?.has(name)? "input-error" : ""} type="text" id={name} disabled={isLoading}/>
                 </div>
+                { emailErrors?.has(name)? (
+                    <p>{emailErrors.get(name)}*</p>
+                    ) : (
+                        <></>
+                    )
+                }
             </div>
         </>
     )
 }
 
-const ContactContent: React.FC = () => {
+interface CsrfContextType {
+    csrfToken: string | null
+    decodeCookie: (cookie: string) => void
+    getCsrf: () => Promise<string | undefined>
+}
+
+type HandleEmailSubmitType = (
+    params: {
+        e: React.FormEvent<HTMLFormElement>,
+        setIsLoading: React.Dispatch<React.SetStateAction<boolean>>,
+        setIsLimit: React.Dispatch<React.SetStateAction<boolean>>,
+        setEmailMessage: React.Dispatch<React.SetStateAction<string>>,
+        setEmailErrors: React.Dispatch<React.SetStateAction<Map<string, string> | null>>,
+        csrfContext: CsrfContextType | null,
+        newCsrf: null | string | undefined,
+        retry: boolean,
+    }
+) => void
+
+interface EmailFormElements extends HTMLFormControlsCollection {
+  firstName: HTMLInputElement;
+  lastName: HTMLInputElement;
+  phoneNumber: HTMLInputElement;
+  email: HTMLInputElement;
+  company: HTMLInputElement;
+  websiteType: HTMLInputElement;
+  message: HTMLTextAreaElement;
+
+}
+
+interface EmailForm extends HTMLFormElement {
+  elements: EmailFormElements;
+}
+
+interface DataErrors {
+    errors: {
+        type: string,
+        value: string,
+        msg: string,
+        path: string,
+        location: string
+    }[]
+}
+
+interface EmailErrorType {
+    errors:{
+        msg: string
+    }[]
+}
+
+
+const handleEmailSubmit:HandleEmailSubmitType = async(params) => {
+    params.e.preventDefault()
+    const form = params.e.target as EmailForm
+    const {firstName, lastName, phoneNumber, email, company, websiteType, message } = form.elements
+    const axiosBody = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        phoneNumber: phoneNumber.value,
+        email: email.value,
+        company: company.value,
+        websiteType: websiteType.value,
+        message: message.value
+    }
+    try{
+        params.setIsLoading(true)
+        const res = await axios.post(`${api.url}/api/email`, 
+            axiosBody,
+            {
+                headers: {
+                    csrftoken: params.newCsrf? params.newCsrf : params.csrfContext?.csrfToken
+                }
+            }
+        )
+        params.setEmailErrors(null)
+        Array.from(form.elements).forEach((element) => {
+            if(element instanceof HTMLInputElement || element instanceof HTMLTextAreaElement){
+                element.value = ""
+            }
+        })
+        const responseMessage = res.data[0].msg
+        params.setEmailMessage(responseMessage)
+        params.setIsLimit(true)
+    } catch(error) {
+        console.error(error)
+        const axiosError = error as AxiosError
+        if(axiosError.status === 403 && params.retry){
+            const newCsrf = await params.csrfContext?.getCsrf()
+            await handleEmailSubmit(
+                {
+                    e: params.e, 
+                    setIsLoading: params.setIsLoading, 
+                    setIsLimit: params.setIsLimit, 
+                    setEmailMessage: params.setEmailMessage, 
+                    setEmailErrors: params.setEmailErrors, 
+                    csrfContext: params.csrfContext, 
+                    newCsrf: newCsrf, 
+                    retry: false
+            })
+        } else if(axiosError.status === 401 && params.retry){
+            await axios.get(`${api.url}/api/auth`)
+            await handleEmailSubmit(
+                {
+                    e: params.e, 
+                    setIsLoading: params.setIsLoading, 
+                    setIsLimit: params.setIsLimit, 
+                    setEmailMessage: params.setEmailMessage, 
+                    setEmailErrors: params.setEmailErrors, 
+                    csrfContext: params.csrfContext, 
+                    newCsrf: null, 
+                    retry: true
+            })
+        } else if (axiosError.status === 400){
+            const axiosData = axiosError.response?.data as DataErrors
+            const errors = axiosData.errors.map(({msg, path}) => {
+                return [path, msg] as [string, string]
+            })
+            params.setEmailErrors(new Map(errors))
+        } else if (axiosError.status === 429){
+            const axiosData = axiosError.response?.data as EmailErrorType
+            const error = axiosData.errors[0].msg
+            params.setEmailMessage(error)
+            params.setIsLimit(true)
+        }
+    } finally {
+        params.setIsLoading(false)
+    }
+}
+
+interface ContactContentProps {
+    setIsLimit: React.Dispatch<React.SetStateAction<boolean>>,
+    setEmailMessage: React.Dispatch<React.SetStateAction<string>>
+}
+
+const ContactContent: React.FC<ContactContentProps> = ({setIsLimit, setEmailMessage}) => {
+    const [ emailErrors, setEmailErrors ] = useState<Map<string, string> | null>(null)
+    const [ isLoading, setIsLoading ] = useState<boolean>(false)
+    const csrfContext = useContext(CsrfContext)
     return(
         <>
             <div className="contact-content__content">
@@ -61,37 +210,80 @@ const ContactContent: React.FC = () => {
                     />
 
                 </div>
-                <form className="contact-content__content-element">
+                <form 
+                    className="contact-content__content-element"
+                    onSubmit={
+                        (e) => handleEmailSubmit(
+                            {
+                                e: e, 
+                                setIsLoading: setIsLoading, 
+                                setIsLimit: setIsLimit, 
+                                setEmailMessage: setEmailMessage, 
+                                setEmailErrors: setEmailErrors, 
+                                csrfContext: csrfContext, 
+                                newCsrf: null, 
+                                retry: true
+
+                        })}
+                >
                     <FormInputElement 
                         name="firstName"
                         text="First Name"
+                        emailErrors={emailErrors}
+                        isLoading={isLoading}
                     />
                     <FormInputElement 
                         name="lastName"
                         text="Last Name"
+                        emailErrors={emailErrors}
+                        isLoading={isLoading}
                     />
                     <FormInputElement 
-                        name="phone"
+                        name="phoneNumber"
                         text="Phone"
+                        emailErrors={emailErrors}
+                        isLoading={isLoading}
                     />
                      <FormInputElement 
                         name="email"
                         text="Email"
+                        emailErrors={emailErrors}
+                        isLoading={isLoading}
                     />
                      <FormInputElement 
                         name="company"
                         text="Company"
+                        emailErrors={emailErrors}
+                        isLoading={isLoading}
                     />
                      <FormInputElement 
                         name="websiteType"
                         text="Website Type"
+                        emailErrors={emailErrors}
+                        isLoading={isLoading}
                     />
                     <div className="contact__form-textarea">
                         <label htmlFor="message">Message</label>
+                        { emailErrors?.has("message")? (
+                            <p>{emailErrors.get("message")}*</p>
+                            ) : (
+                                <></>
+                            )
+                        }
                         <div>
-                            <textarea name="message" id="message"></textarea>
+                            <textarea disabled={isLoading} className={emailErrors?.has("message")? "input-error" : ""} name="message" id="message"></textarea>
+
                         </div>
                     </div>
+                    <button disabled={isLoading}>
+                        {isLoading? (
+                            <AiOutlineLoading />
+                        ) : (
+                            <>
+                                SUBMIT
+                            </>
+                        )}
+                    </button>
                 </form>
             </div>
         </>
--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -3,15 +3,36 @@ import '../../assets/styles/contact/contact.css'
 import FooterHV from "../../components/universal/footer"
 import ContactHeader from "../../components/contact/contactHeader"
 import ContactContent from "../../components/contact/contactContent"
-
+import { useState, useEffect } from "react"
+import EmailMessage from "../../components/errors/emailMessage"
 const Contact: React.FC = () => {
+    const [ isLimit, setIsLimit ] = useState<boolean>(false)
+    const [ emailMessage, setEmailMessage ] = useState<string>("")
+
+    useEffect(() => {
+        if(isLimit){
+            const timeoutId = setTimeout(() => {
+                setIsLimit(false)
+            }, 8000)
+
+            return () => clearTimeout(timeoutId)
+        }
+    }, [isLimit])
     return(
         <>
+            {isLimit && (
+                <EmailMessage 
+                    message={emailMessage}
+                />
+            )}
             <NavBar />
             <main className="page-section">
                 <section className="page-section__child contact-content">
                     <ContactHeader />
-                    <ContactContent />
+                    <ContactContent 
+                        setIsLimit={setIsLimit}
+                        setEmailMessage={setEmailMessage}
+                    />
                 </section>
             </main>
             <footer className="contact-footer">
