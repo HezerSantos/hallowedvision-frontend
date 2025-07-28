f0a799a91df39ba06663873861942ca08d04530a
HezerSantos <HezerSantos24@gmail.com>
2025-07-27 20:39:40 -0500

    SEO imporvements

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -22,8 +22,7 @@ const Contact: React.FC = () => {
     return(
         <>
             <Helmet>
-                <title>Contact Us | HallowedVisions</title>
-                <title>Contact & Pricing | Hallowed Visions – Custom Web Design Solutions</title>
+                <title>Contact & Pricing | Hallowed Visions</title>
                 <meta name="title" content="Contact & Pricing | Hallowed Visions – Custom Web Design Solutions"/>
                 <meta name="description" content="Get in touch with Hallowed Visions to discuss your custom web design needs. Flexible pricing based on team size, integrations, and project complexity. Let's create together."/>
                 <meta name="keywords" content="Hallowed Visions, web design, pricing, contact, custom websites, digital solutions, creative agency, project quote, website development, onboarding support"/>
